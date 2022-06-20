7eac3769c72f2bfdfa26086ad6121c48800956d5
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-06-19 21:03:54 -0500

    return messages

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Code your solutions in this file
 
+let messages = []
+
 const writeCards = (array, event) => {
     for (let i = 0; i < array.length; i++) {
-        
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
     }
+
+    return messages
 }
